fdbf8c497775f65b1cbda9179022691409676732
Melanie Bockmann <melanie.bockmann@gmail.com>
2018-06-15 19:08:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center">&lt;CSS/&gt;</h1>
+<br/>
+CSS generator for web developers and designers
+<br/>
+<br/>
+
+## LICENSE
+
+Licensed under the [MIT License](https://github.com/Parasail-Health/parasail/blob/master/LICENSE.txt).
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './assets/css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>&lt; CSS /&gt;</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
